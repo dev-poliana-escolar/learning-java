a856b676b961a7055caba1e904154de26521ea62
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-04-27 16:49:17 -0300

    feat: soma de vetor recursiva

---
+++ ListaExercicio/src/listaexercicio/Questao1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listaexercicio;
+
+//import java.util.Scanner;
+
+public class Questao1 {
+ 
+    //recursiva: somar até esvaziar o vetor
+    public static int somaVetor(int listaNum[], int tamanho){
+        if (tamanho==0){
+            return 0;
+        }
+        return listaNum[tamanho-1] + somaVetor(listaNum, tamanho-1);
+    }
+    
+    public static void main(String[] args) {
+        int l[] ={10,2};
+        System.out.println("Resultado: " + somaVetor(l, l.length));
+    }
+}
